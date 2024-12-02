0d3a485b425150d8291811ae22361e27804a1557
puneet3663 <89412789+puneet3663@users.noreply.github.com>
2024-12-02 15:05:24 +0530

    Update sparkcode.py

--- sparkcode.py
+++ sparkcode.py
@@ -38,3 +38,115 @@ joined_df = df1.join(df2, "id").join(df3.hint("broadcast"), "id")
 
 joined_df.show()
 
+##################### Distinct function #######################
+
+distinctDF = df.distinct()
+print("Distinct count: "+str(distinctDF.count()))
+distinctDF.show(truncate=False)
+
+##################### Drop Duplicates #######################
+df2 = df.dropDuplicates()
+print("Distinct count: "+str(df2.count()))
+df2.show(truncate=False)
+
+##################### Drop Duplicates on 2 columns only #######################
+dropDisDF = df.dropDuplicates(["department","salary"])
+print("Distinct count of department & salary : "+str(dropDisDF.count()))
+dropDisDF.show(truncate=False)
+
+##################### repartition #######################
+df2 = df.repartition(6)
+print(df2.rdd.getNumPartitions())
+
+################ UDF example ############################
+
+# reference link from where code is taken https://sparkbyexamples.com/pyspark/pyspark-udf-user-defined-function/
+       
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+columns = ["Seqno","Name"]
+data = [("1", "john jones"),
+    ("2", "tracey smith"),
+    ("3", "amy sanders")]
+
+df = spark.createDataFrame(data=data,schema=columns)
+
+df.show(truncate=False)
+
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import StringType
+
+def convertCase(str):
+    resStr=""
+    arr = str.split(" ")
+    for x in arr:
+       resStr= resStr + x[0:1].upper() + x[1:len(x)] + " "
+    return resStr 
+
+spark.udf.register("convertUDF", convertCase,StringType())
+df.createOrReplaceTempView("NAME_TABLE")
+spark.sql("select Seqno, convertUDF(Name) as Name from NAME_TABLE") \
+     .show(truncate=False)
+
+#output is 
+Seqno|Name         |
++-----+-------------+
+|1    |John Jones   |
+|2    |Tracey Smith |
+|3    |Amy Sanders  |
++-----+-------------+
+         
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
+
+############################################
